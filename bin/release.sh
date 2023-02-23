02e4444c09fecd96c530092fc38a32d5d7a7e348
Michael Kreil <github@michael-kreil.de>
2023-02-23 23:37:26 +0100

    add release action

--- bin/release.sh
+++ bin/release.sh
@@ -12,7 +12,3 @@ cd ..
 
 echo " -> gzip"
 gzip -9kfv styles.tar
-
-echo " -> release"
-release_version="v$(jq -r '.version' ../package.json)"
-gh release create $release_version --generate-notes styles.tar.gz
