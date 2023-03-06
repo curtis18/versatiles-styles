2fc5f401415eb3e51724d1cbc8967bba1e882ea8
Michael Kreil <github@michael-kreil.de>
2023-03-06 16:38:59 +0100

    notes

--- bin/generate.sh
+++ bin/generate.sh
@@ -10,9 +10,9 @@ function copy {
 	echo " -> copy $1"
 	mkdir -p dist/styles/$1/
 	cp -X src/$1/*.* dist/styles/$1/
+	node bin/minify.js dist/styles/$1/style.json
 }
 
+copy eclipse
 copy neutrino
 copy shortbread
-
-find dist -name "*.json" -exec node bin/minify.js {} +
--- bin/minify.js
+++ bin/minify.js
@@ -2,8 +2,16 @@
 import { argv } from 'node:process';
 import { readFileSync, writeFileSync } from 'node:fs';
 
+// use https://www.npmjs.com/package/colord
+
 argv.slice(2).forEach(filename => {
 	console.log(`minify ${filename}`);
-	let data = JSON.parse(readFileSync(filename,'utf8'));
-	writeFileSync(filename,JSON.stringify(data),'utf8');
+	let data = JSON.parse(readFileSync(filename, 'utf8'));
+
+	// fix glyphs path
+	// fix sprite path
+	// fix font name
+	// create stylemaker-wrapper
+
+	writeFileSync(filename, JSON.stringify(data), 'utf8');
 });
